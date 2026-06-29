427292c05b766ce759747b36ae8fc7041af9d530
SMZulqarnain <smzulqi312@gmail.com>
2026-06-29 17:36:24 +0900

    Remove kdu-backend and kdu-website folders

--- kdu-website/src/constants/links.js
+++
@@ -1,2 +0,0 @@
-export const APPLICATION_FORM_URL =
-  'https://forms.office.com/pages/responsepage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAN__4ZCI6ZUM1JVVDJYWFJHVzhYR0lWNUFHUDhVWkVWTC4u&route=shorturl';
